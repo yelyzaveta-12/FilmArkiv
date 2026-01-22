3577e7562835baac279352fc85677b6e2e39816e
yelyzaveta-12 <lagnyuk.liza@gmail.com>
2026-01-22 18:49:40 +0100

    just outprinting in tekstgrensesnitt

--- src/no/hvl/data102/filmarkiv/klient/Tekstgrensesnitt.java
+++ src/no/hvl/data102/filmarkiv/klient/Tekstgrensesnitt.java
@@ -41,21 +41,37 @@ public class Tekstgrensesnitt {
     }
     // Skriver ut en film med alle opplysninger på skjerm (husk tekst for sjanger)
     public void skrivUtFilm(Film film) {
-// TODO
+
+        if(film != null){
+            System.out.println("Filmnr: " + film.getFilmnr()
+            + ", Produsent: " + film.getProdusent()
+                    + "Tittel: " + film.getTittel()
+                    + "Aar: " + film.getAar()
+                    + "Sjanger: " + film.getSjanger() +
+                    "Filmselskap: " + film.getFilmselskap()
+            );
+        }
     }
+
+
     // Skriver ut alle filmer med en spesiell delstreng i tittelen
     public void skrivUtFilmDelstrengITittel(FilmarkivADT arkiv, String delstreng) {
-// TODO
+
+
     }
+
+    
     // Skriver ut alle Filmer av en produsent (produsent er delstreng)
     public void skrivUtFilmProdusent(FilmarkivADT arkiv, String delstreng) {
 // TODO
     }
+
+
     // Skriver ut en enkel statistikk som inneholder antall filmer totalt
 // og hvor mange det er i hver sjanger.
     public void skrivUtStatistikk(FilmarkivADT arkiv) {
 // TODO
     }
-// osv ... andre metode
+
     scanner.close();
 }
